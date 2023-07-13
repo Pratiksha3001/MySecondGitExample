f957e703180dbbb8cd680069addd8cb664024e09
pratiksha <pratikshagholap77@gmail.com>
2023-07-13 15:55:49 +0530

    First New Commit

---
+++ src/com/practice/git/Demo.java
@@ -0,0 +1,12 @@
+package com.practice.git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 
+		 System.out.println("Welcome to git Test Class");
+
+		 System.out.println("done!!!!");
+	}
+
+}
---
+++ src/com/practice/git/Test.java
@@ -0,0 +1,16 @@
+package com.practice.git;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 ArrayList al=new ArrayList();
+         al.add(20);
+         al.add(null);
+         al.add(null);
+         al.add("Vishal");
+         System.out.println(al);
+	}
+
+}
